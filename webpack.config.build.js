e15b273750fc8ddd3414218e292aa3dbf6bf0ede
Maciej Piernikowski <maciej@piernikowski.net>
2018-08-29 15:28:22 +0200

    [FON-1] added deploy script

--- webpack.config.build.js
+++ webpack.config.build.js
@@ -2,7 +2,10 @@ const path = require('path');
 const merge = require('webpack-merge');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const CopyWebpackPlugin = reqire('copy-webpack-plugin');
+const SshWebpackPlugin = require('ssh-webpack-plugin');
 const webpackConfig = require('./webpack.config');
+
+const shhPassword = process.env.arg
 
 module.exports = merge(webpackConfig, {
 
@@ -23,6 +26,17 @@ module.exports = merge(webpackConfig, {
             {
                 from: path.join(__dirname, 'assets/tags/*'),
                 to: path.join(__dirname, 'dist/assets/fonts')
+            }
+        ]),
+        new SshWebpackPlugin([
+            {
+                host: 'fonts.perfecthair.ch',
+                port: '22',
+                username: 'maciej',
+                password: '***',
+                privateKey: require('fs').readFileSync('/Volumes/Storage/.ssh/maciej'),
+                from: path.join(__dirname, 'dist'),
+                to: '/'
             }
         ])
     ]
